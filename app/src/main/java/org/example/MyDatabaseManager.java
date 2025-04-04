d1c3d455034bd735edda420fafdd75e59c35c4e3
Youngho Cho <dominoyh5@gmail.com>
2025-04-04 13:30:01 +0900

    very big db file

--- app/src/main/java/org/example/MyDatabaseManager.java
+++ app/src/main/java/org/example/MyDatabaseManager.java
@@ -29,13 +29,13 @@ class MyDatabaseManager implements DatabaseManager {
 
         // find last record
         Pointer lastRecordPointer = findLastRecordPointer(blockManager, metadata);
+        Record lastRecord = new Record(blockManager, metadata.getFields(), lastRecordPointer);
 
         // determine next record position
         Pointer newPointer = new Pointer(1, 0);
         if (lastRecordPointer.isNullPointer()) {
             metadata.setFirstRecordPointer(newPointer);
         } else {
-            Record lastRecord = new Record(blockManager, metadata.getFields(), lastRecordPointer);
             final int usedSpace = lastRecordPointer.getOffset() + lastRecord.size();
             final int freeSpace = BLOCK_SIZE - usedSpace;
 
@@ -49,6 +49,10 @@ class MyDatabaseManager implements DatabaseManager {
 
         // insert new record
         metadata.write(headerBlock);
+
+        lastRecord.setNextPointer(Optional.of(newPointer));
+        // lastRecord.write(blockManager, metadata.getFields(), lastRecordPointer); // FIXME: wrong pointer?
+
         newRecord.setNextPointer(Optional.of(new Pointer(0, 0)));
         newRecord.write(blockManager, metadata.getFields(), newPointer);
     }
